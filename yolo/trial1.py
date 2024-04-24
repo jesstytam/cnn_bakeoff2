478a293c6d3267e84729dbe8d08b96f1dd8dd034
jesstytam <j.tam@unsw.edu.au>
2024-04-24 10:33:09 +1000

    adding some data

---
+++ yolo/trial1.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# Load a COCO-pretrained YOLOv5n model
+model = YOLO('yolov5l.pt')
+
+# Train the model on the COCO8 example dataset for 100 epochs
+results = model.train(data='config/trial1.yaml', epochs=100, imgsz=640)
+
+# Run inference with the YOLOv5n model on the 'bus.jpg' image
+results = model('path/to/bus.jpg')
